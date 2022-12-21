b37b9b7b4f66f0fc29b616f4f014269ed13ba3b7
55973 <55973@etu.he2b.be>
2022-12-21 09:47:04 +0100

    while et for

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,15 @@ for (let index = 1; index < 11; index++) {
     console.log(index);
     
 }
+
+console.log('');
+console.log('While');
+
+let i = 10;
+while(i>0){
+    console.log(i);
+    i--;
+}
+
+console.log('');
+console.log('While');
