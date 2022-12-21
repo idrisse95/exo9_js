ca4bc18f247deb4627b7eb377c23df71ff08e54e
55973 <55973@etu.he2b.be>
2022-12-21 10:34:40 +0100

    fin

--- public/js/main.js
+++ public/js/main.js
@@ -1,87 +1,87 @@
-let un =0
-for (let index = 1; index < 11; index++) {
-    console.log(index);
+// let un =0
+// for (let index = 1; index < 11; index++) {
+//     console.log(index);
     
-}
-
-console.log('');
-console.log('While');
-
-let sum = 10;
-while(sum>0){
-    console.log(sum);
-    sum--;
-}
-
-console.log('');
-console.log('Do Wdhile');
-
-let j = 2
-do {
-    console.log(j);
-    j=j+2;
-} while (j<22);
-
-console.log('');
-console.log('For of');
-
-let tab = [2,3,4,5,6,4,"idriss"]
-
-for (const iterator of tab) {
-    console.log(iterator);
-}
+// }
 
 // console.log('');
-// console.log('For Deux');
+// console.log('While');
 
-
-// for (let sum = 0; sum < 10; sum++) {
+// let sum = 10;
+// while(sum>0){
 //     console.log(sum);
-//     sum=sum+sum;
-    
+//     sum--;
 // }
 
-// let nb = +prompt('entre un nb pos')
-// if (nb<0) {
+// console.log('');
+// console.log('Do Wdhile');
+
+// let j = 2
+// do {
+//     console.log(j);
+//     j=j+2;
+// } while (j<22);
+
+// console.log('');
+// console.log('For of');
+
+// let tab = [2,3,4,5,6,4,"idriss"]
+
+// for (const iterator of tab) {
+//     console.log(iterator);
+// }
+
+// // console.log('');
+// // console.log('For Deux');
+
+
+// // for (let sum = 0; sum < 10; sum++) {
+// //     console.log(sum);
+// //     sum=sum+sum;
+    
+// // }
+
+// // let nb = +prompt('entre un nb pos')
+// // if (nb<0) {
  
-//            prompt('entre positif')
+// //            prompt('entre positif')
       
-// }
-// else{
-//     alert('c bon')
-// }
+// // }
+// // else{
+// //     alert('c bon')
+// // }
+
+
+// // console.log('');
+// // console.log('For 6');
+// // for (let nbD = 0; nbD < 10;nbD++) {
+ 
+// //     console.log(nbD);
+
+// // switch (true) {
+// //     case nbD==5:
+// //         alert('stop')
+// //         break;
+
+// //     default:
+// //         break;
+// // }}
+
 
 
 // console.log('');
-// console.log('For 6');
-// for (let nbD = 0; nbD < 10;nbD++) {
- 
-//     console.log(nbD);
+// console.log('For 7');
 
-// switch (true) {
-//     case nbD==5:
-//         alert('stop')
-//         break;
-
-//     default:
-//         break;
-// }}
-
-
-
-console.log('');
-console.log('For 7');
-
-for (let paire = 0; paire < 11; paire++) {
-    if (paire % 2 == 0) {
-        console.log(paire+ 'oui');
+// for (let paire = 0; paire < 11; paire++) {
+//     if (paire % 2 == 0) {
+//         console.log(paire+ 'oui');
         
-    }
-    else{
-        console.log(paire+'non paire');
-    }
+//     }
+//     else{
+//         console.log(paire+'non paire');
+//     }
     
-}
+// }
 
 
 
@@ -97,22 +97,22 @@ for (let paire = 0; paire < 11; paire++) {
 
 
 
-// bonus
+// // bonus
 
-console.log('');
-console.log('BONUS');
+// console.log('');
+// console.log('BONUS');
 
 
 
-let bon = []
-for (let iB = 0; iB < 10; iB++) {
-    let nbB = +prompt('entre 10 chifr')
-    bon.push(nbB);
+// let bon = []
+// for (let iB = 0; iB < 10; iB++) {
+//     let nbB = +prompt('entre 10 chifr')
+//     bon.push(nbB);
    
     
-}
-alert(bon)
+// }
+// alert(bon)
 
-let qstn = +prompt('quel nbr cherche tu?')
-alert(bon.indexOf(qstn));
+// let qstn = +prompt('quel nbr cherche tu?')
+// alert(bon.indexOf(qstn));
 
